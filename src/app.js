4dc88474f244c5f530d3f9f18f4b09739175e0f8
Pranav Prakash <pranav.prakash@bookmyshow.com>
2017-10-14 21:53:36 +0530

    :hammer: Uses official Koa cors package
    Uses official Koa cors package

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-import Koa from "koa";
-import cors from "kcors";
-import logger from "koa-morgan";
-import bodyParser from "koa-bodyparser";
-import router from "./routes";
+import Koa from 'koa';
+import cors from '@koa/cors';
+import logger from 'koa-morgan';
+import bodyParser from 'koa-bodyparser';
+import router from './routes';
 
 const app = new Koa();
 
 // Set middlewares
 app.use(
-    bodyParser({
-        enableTypes: ["json", "form"],
-        formLimit: "10mb",
-        jsonLimit: "10mb"
-    })
+  bodyParser({
+    enableTypes: ['json', 'form'],
+    formLimit: '10mb',
+    jsonLimit: '10mb'
+  })
 );
 
 // Logger
 app.use(
-    logger("dev", {
-        skip: () => app.env === "test"
-    })
+  logger('dev', {
+    skip: () => app.env === 'test'
+  })
 );
 
 // Enable CORS
@@ -27,19 +27,19 @@ app.use(cors());
 
 // Default error handler middleware
 app.use(async (ctx, next) => {
-    try {
-        await next();
-        if (ctx.status === 404) {
-            ctx.throw(404);
-        }
-    } catch (err) {
-        ctx.status = err.statusCode || err.status || 500;
-        ctx.body = {
-            statusCode: ctx.status,
-            message: err.message
-        };
-        ctx.app.emit("error", err, ctx);
+  try {
+    await next();
+    if (ctx.status === 404) {
+      ctx.throw(404);
     }
+  } catch (err) {
+    ctx.status = err.statusCode || err.status || 500;
+    ctx.body = {
+      statusCode: ctx.status,
+      message: err.message
+    };
+    ctx.app.emit('error', err, ctx);
+  }
 });
 
 // Routes
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import app from "./app";
+import app from './app';
 
 const { PORT = 8080 } = process.env;
 const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
